83f3240f043411fc6fd2167105574abfb6b7f60d
juliasjogren <juliaadelasjogren@gmail.com>
2018-01-17 13:19:25 +0100

    new files with basic styling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hej')
+let pages = document.getElementsByClassName('page')
+
+function navClick (pageName) {
+  for (let page of pages) {
+    page.classList.add('hidden')
+  }
+  document.getElementById(pageName).classList.remove('hidden')
+}
